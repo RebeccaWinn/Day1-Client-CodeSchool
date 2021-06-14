0268e03b5e31f9dd8f948b528c25d31c13cb9d51
rwinn <d00336896@dmail.dixie.edu>
2021-06-14 13:42:05 -0600

    6/14/2021

---
+++ practice/app.js
@@ -0,0 +1,9 @@
+var app= new Vue ({
+    el:"#app",
+    data:{
+        locations:[
+            { text:"Saint George,UT" },
+            { text:"Los Angeles, CA" }
+        ]
+    }
+});
---
+++ quiz/app.js
@@ -0,0 +1,46 @@
+
+var quiz= {
+    title:"My quiz",
+    questions:[
+        {
+            text:"Question 1",
+            responses:[
+                {text: "Wrong, too bad"},
+                {text: "Right!", correct:true},
+            ]
+        },
+        {
+            text:"Question 2",
+            responses:[
+                {text: "Right answer", correct:true},
+                {text: "Wrong answer"},
+            ]
+        }
+    ]
+};
+
+
+
+
+var app = new Vue({
+    el: "#app",
+    data:{
+        quiz:quiz,
+        questionIndex:0,
+        userResponses:Array(quiz.questions.length).fill(false)
+    },
+    methods:{
+        next:function(){
+            this.questionIndex++;
+        },
+
+        prev:function(){
+            this.questionIndex--;
+        },
+
+        score:function(){
+            return this.userResponses.filter(function(val){return val}).length
+        }
+
+    }
+});
